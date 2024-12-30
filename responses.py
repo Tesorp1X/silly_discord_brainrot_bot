213590884eccf0bbdde9ee2fe28accbdbf44cba8
Tesorp1X <rakteam2014@gmail.com>
2024-12-31 03:34:52 +0700

    New responses: 'now playing' and 'list queue'.

--- responses.py
+++ responses.py
@@ -3,8 +3,9 @@ from discord.utils import get
 from discord import FFmpegPCMAudio
 
 from youtube_dl import YoutubeDL
+import MusicQueue
 
-def get_response(user_input: str) -> str:
+def get_default_response(user_input: str) -> str:
     lowered: str = user_input.lower()
 
     if lowered == '':
@@ -13,4 +14,38 @@ def get_response(user_input: str) -> str:
         return 'ohayo!'
     else:
         return 'chippi chippi chapa chapa'
+
+"""
+    Discord Markdown Format:
+    -Italics	*italics* or _italics_	Underline italics	__*underline italics*__
+    -Bold	**bold**	Underline bold	__**underline bold**__
+    -Bold Italics	***bold italics***	underline bold italics	__***underline bold italics***__
+    -Underline	__underline__	Strikethrough	 ~~Strikethrough~~
+    -Headers # ## ###
+    -Masked Links [Text](link)
+    -Lists - *
+"""
+
+def get_new_song_response(song_title: str, yt_link: str, next_song_title: str) -> str:
+
+    now_playing_text = f"**Сейчас играет: [{song_title}]({yt_link})**"
+    next_song_text = f"**Следующая песня:** *{next_song_title}*"
+    return now_playing_text + '\n' + next_song_text
+
+
+def get_queue_list_response(queue: list[MusicQueue.QueueItem]) -> str:
+    next_in_queue_txt = "**Очередь:**"
+    list_txt = ""
+    item_no = 0
+    for item in queue:
+        item_no += 1
+        song_name = item.get_title()
+        yt_link = item.get_yt_link()
+        if list_txt != "":
+            list_txt = list_txt + '\n' + \
+                        f"{item_no}. [{song_name}]({yt_link})"
+        else:
+            list_txt = f"{item_no}. [{song_name}]({yt_link})"
+
+    return next_in_queue_txt + "\n" + list_txt
 
