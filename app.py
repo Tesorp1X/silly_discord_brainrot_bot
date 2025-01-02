b59e43b2b3c7e07cbb98d2b685d61e191348ed30
Tesorp1X <rakteam2014@gmail.com>
2025-01-02 19:15:25 +0700

    Bug fix: play.before_invoke (ensure_voice) should work rn.

--- app.py
+++ app.py
@@ -91,7 +91,8 @@ async def play_all(ctx: Context) -> None:
 
 
 @bot.command(name="play")
-async def play(ctx: Context, url: str) -> None:
+async def play(ctx: Context) -> None:
+  url: str = ctx.message.content
   with YoutubeDL(YDL_OPTS) as ydl:
     info = ydl.extract_info(url, download=False)
     video_link = info.get('url') #direct link, that is going to be used by FFMPEG
